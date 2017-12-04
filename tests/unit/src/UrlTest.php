2052d17f031dfb38b493cf5fff83339f5894ca17
Vladan <vladan.zoricic@gmail.com>
2017-12-04 11:17:40 +0100

    000 - Added Port value object into testPort method, added Dictionary object into testQuery method and isntead of passing array as parameter into path method, string values are passed.

--- tests/unit/src/UrlTest.php
+++ tests/unit/src/UrlTest.php
@@ -2,6 +2,8 @@
 
 use G4\ValueObject\Url;
 use G4\ValueObject\Exception\InvalidUrlException;
+use G4\ValueObject\PortNumber;
+use G4\ValueObject\Dictionary;
 
 class UrlTest extends \PHPUnit_Framework_TestCase
 {
@@ -23,20 +25,20 @@ class UrlTest extends \PHPUnit_Framework_TestCase
     {
         $url = new Url('http://subdomain.domain.com');
 
-        $this->assertEquals('http://subdomain.domain.com/path1/path2', (string) $url->path(['path1', 'path2']));
+        $this->assertEquals('http://subdomain.domain.com/path1/path2', (string) $url->path('path1', 'path2'));
     }
 
     public function testPort()
     {
         $url = new Url('http://subdomain.domain.com');
 
-        $this->assertEquals('http://subdomain.domain.com:8080', (string) $url->port('8080'));
+        $this->assertEquals('http://subdomain.domain.com:8080', (string) $url->port(new PortNumber('8080')));
     }
 
     public function testQuery()
     {
         $url = new Url('http://subdomain.domain.com');
 
-        $this->assertEquals('http://subdomain.domain.com?query1=test1&query2=test2', (string) $url->query(['query1' => 'test1', 'query2' => 'test2']));
+        $this->assertEquals('http://subdomain.domain.com?query1=test1&query2=test2', (string) $url->query(new Dictionary(['query1' => 'test1', 'query2' => 'test2'])));
     }
 }
