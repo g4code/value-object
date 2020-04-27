453e6f51e3585e1d35c08460060d3612a6fdeb5f
Sanja Medic <sanja.medic@codeplicity.com>
2020-04-27 15:38:20 +0200

    000 - added vat for GB and US country code

--- tests/unit/src/VatTest.php
+++ tests/unit/src/VatTest.php
@@ -11,18 +11,20 @@ class VatTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals(22, (new Vat(new CountryCode('IT')))->get());
         $this->assertEquals(20, (new Vat(new CountryCode('FR')))->get());
+        $this->assertEquals(0, (new Vat(new CountryCode('US')))->get());
     }
 
     public function testToString()
     {
         $this->assertEquals('22%', (string)new Vat(new CountryCode('IT')));
         $this->assertEquals('20%', (string)new Vat(new CountryCode('FR')));
+        $this->assertEquals('0%', (string)new Vat(new CountryCode('US')));
     }
 
     public function testException()
     {
         $this->expectException(MissingVatException::class);
-        $this->expectExceptionMessage('Vat not set for country code: GB');
-        new Vat(new CountryCode('GB'));
+        $this->expectExceptionMessage('Vat not set for country code: RS');
+        new Vat(new CountryCode('RS'));
     }
 }
